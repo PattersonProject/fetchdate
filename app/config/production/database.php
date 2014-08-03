a5b3a41ffd2ab2a55b0d5b177d5833477b0e8793
Michael Patterson <michael.pttrson@gmail.com>
2014-08-03 08:10:49 -0400

    Updated Boxfile for pagoda

---
+++ app/config/production/database.php
@@ -0,0 +1,17 @@
+<?php
+
+return array(
+
+    'connections' => array(
+
+        'mysql' => array(
+            'driver'    => 'mysql',
+            'host'      => 'tunnel.pagodabox.com:3306',
+            'database'  => 'fetchdate',
+            'username'  => 'marian',
+            'password'  => '4VYiMLkA',
+        ),
+
+    ),
+
+);
