3ac5dc8e626d376bad1304f3dec21af545284b3a
Michael Patterson <michael.pttrson@gmail.com>
2014-08-07 09:09:22 -0400

    Change database config for OpenShift

--- app/config/production/database.php
+++ app/config/production/database.php
@@ -4,6 +4,7 @@ return array(
 
     'connections' => array(
 
+        // Fetchdate 2  pagoda database
         // 'mysql' => array(
         //     'driver'    => 'mysql',
         //     'host'      => 'tunnel.pagodabox.com:3306',
@@ -12,13 +13,24 @@ return array(
         //     'password'  => 'gZOuVhlc',
         // ),
 
+        // Fetchdate pagoda database
+        // 'mysql' => array(
+        //     'driver'    => 'mysql',
+        //     'host'      => 'tunnel.pagodabox.com:3306',
+        //     'database'  => 'fetchdate',
+        //     'username'  => 'marian',
+        //     'password'  => '4VYiMLkA',
+        // ),
+
+        // Openshift database
         'mysql' => array(
             'driver'    => 'mysql',
-            'host'      => 'tunnel.pagodabox.com:3306',
+            'host'      => '127.10.20.130',
             'database'  => 'fetchdate',
-            'username'  => 'marian',
-            'password'  => '4VYiMLkA',
+            'username'  => 'admin4KQfbkt',
+            'password'  => 'HtCka9JVk3EI',
         ),
+
 
     ),
 
